ec16dbb23fa78ad8955d444f812b85d59d619f7b
Chike Nwaenie <chikenwaenie@gmail.com>
2017-03-18 10:15:00 +0000

    Used a loop to increase efficiency

--- findAverage/src/main/java/averageCalculator.java
+++ findAverage/src/main/java/averageCalculator.java
@@ -1,7 +1,7 @@
 /**
  * Created by chike on 16/03/2017.
- * With objects like, Scanner and DecimalFormat, if you open a new 'instance' perhaps, of that object,
- * you can reuse them anywhere in your code, just by using the object with its own specific 'method' perhaps.
+ * With objects like, Scanner and DecimalFormat, if you open a new 'instance', of that object,
+ * you can reuse them anywhere in your code, just by using the object with its own specific method.
  *
  * A double is similar to a float.
  *
@@ -12,33 +12,27 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
-class averageCalculator {
-    public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
+class findTheAverage {
+    public static void main(String args[]){
+        Scanner scan  = new Scanner(System.in);
         DecimalFormat twoDP = new DecimalFormat(".##");
+
+        double sum = 0;
+        double average;
+        int i;
+
+        System.out.println("I will ask you to enter six numbers:");
         try {
-            System.out.print("Please enter your first number: ");
-            double a = scan.nextDouble();
-            System.out.print("Please enter your second number: ");
-            double b = scan.nextDouble();
-            System.out.print("Please enter your third number: ");
-            double c = scan.nextDouble();
-            System.out.print("Please enter your fourth number: ");
-            double d = scan.nextDouble();
-            System.out.print("Please enter your fifth number: ");
-            double e = scan.nextDouble();
-            System.out.print("Please enter your sixth number: ");
-            double f = scan.nextDouble();
+            for (i = 1; i <= 6; i++) {
+                System.out.print(i + ") ");
+                sum += scan.nextDouble();
+            }
 
-            double sum = a + b + c + d + e + f;
-            double average = sum / 6;
-
-            System.out.println("\nThe average of all of your numbers is: " + twoDP.format(average));
+            average = sum / 6;
+            System.out.println("\nThe average all these numbers is: " + twoDP.format(average));
         }
-        catch(InputMismatchException e){
-            System.out.println("Please only enter numerical values.");
+        catch (InputMismatchException e){
+            System.out.println("Ensure all values are numerical.");
         }
-
     }
-
 }
